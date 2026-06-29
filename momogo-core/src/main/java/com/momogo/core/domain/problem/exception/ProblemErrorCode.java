5bb0fa4ded6b9622b329dc124485c2c779c32a0c
Junkov0 <devjuncloud@gmail.com>
2026-06-29 21:59:00 +0900

    feat: add problem service

--- momogo-core/src/main/java/com/momogo/core/domain/problem/exception/ProblemErrorCode.java
+++ momogo-core/src/main/java/com/momogo/core/domain/problem/exception/ProblemErrorCode.java
@@ -12,17 +12,20 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ProblemErrorCode implements ErrorCode {
 
-  PROBLEM_NOT_FOUND(1001, "PROBLEM_NOT_FOUND",
+  PROBLEM_NOT_FOUND(3001, "PROBLEM_NOT_FOUND",
       HttpStatus.NOT_FOUND, "해당 문제를 찾을 수 없습니다."),
 
-  CATEGORY_NOT_FOUND(1002, "CATEGORY_NOT_FOUND",
+  CATEGORY_NOT_FOUND(3002, "CATEGORY_NOT_FOUND",
       HttpStatus.NOT_FOUND, "해당 카테고리를 찾을 수 없습니다."),
 
-  CATEGORY_NAME_DUPLICATED(1003, "CATEGORY_NAME_DUPLICATED",
+  CATEGORY_NAME_DUPLICATED(3003, "CATEGORY_NAME_DUPLICATED",
       HttpStatus.CONFLICT, "이미 존재하는 카테고리 이름입니다."),
 
-  CATEGORY_HAS_PROBLEMS(1004, "CATEGORY_HAS_PROBLEMS",
-      HttpStatus.CONFLICT, "해당 카테고리에 문제가 존재하여 삭제할 수 없습니다.");
+  CATEGORY_HAS_PROBLEMS(3004, "CATEGORY_HAS_PROBLEMS",
+      HttpStatus.CONFLICT, "해당 카테고리에 문제가 존재하여 삭제할 수 없습니다."),
+
+  SPACE_NOT_FOUND(3005, "SPACE_NOT_FOUND",
+      HttpStatus.NOT_FOUND, "해당 공간을 찾을 수 없습니다.");
 
   private final int numeric;
   private final String errorKey;
