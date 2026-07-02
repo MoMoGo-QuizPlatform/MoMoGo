42e527cf8ec87f3d4e5f3cb2d498663229ca5a7e
홍성휘 <gkemg2017@gmail.com>
2026-07-02 14:27:28 +0900

    refactor: @RequestHeader to @AuthenticationPrincipal refactor

--- momogo-api/src/main/java/com/momogo/api/room/RoomController.java
+++ momogo-api/src/main/java/com/momogo/api/room/RoomController.java
@@ -1,5 +1,6 @@
 package com.momogo.api.room;
 
+import com.momogo.api.auth.details.MoMoGoUserDetails;
 import com.momogo.core.domain.room.dto.request.RoomCreateRequest;
 import com.momogo.core.domain.room.dto.response.RoomResponse;
 import com.momogo.core.domain.room.service.RoomService;
@@ -8,6 +9,7 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,14 +24,21 @@ public class RoomController {
 
   private final RoomService roomService;
 
+  /**
+   * 평가 시험 개설
+   * @param userDetails 유저 아이디
+   * @param spaceId 공간 아이디
+   * @param request 평가 시험 개설 요청 DTO
+   * @return 개설된 평가 시험 정보
+   */
   @PostMapping("/spaces/{spaceId}/rooms")
   public ResponseEntity<RoomResponse> createRoom(
-      @RequestHeader("X-User-Id")UUID userId,
+      @AuthenticationPrincipal MoMoGoUserDetails userDetails,
       @PathVariable UUID spaceId,
       @Valid @RequestBody RoomCreateRequest request
   ) {
 
-    RoomResponse response = roomService.createRoom(userId, spaceId, request);
+    RoomResponse response = roomService.createRoom(userDetails.getUserResponse().id(), spaceId, request);
     return ResponseEntity.status(HttpStatus.CREATED).body(response);
   }
 }
