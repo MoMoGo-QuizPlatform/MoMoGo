73758fe69522f0708802f80ac72f312f69860ce1
홍성휘 <gkemg2017@gmail.com>
2026-08-10 17:07:38 +0900

    ci: trigger backend deployment pipeline for multi-instance EC2 servers

--- momogo-api/src/main/java/com/momogo/api/MomogoApiApplication.java
+++ momogo-api/src/main/java/com/momogo/api/MomogoApiApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * MoMoGo API Application Engine
+ * Trigger CI/CD Multi-Instance Production Deployment
+ */
 @SpringBootApplication(scanBasePackages = "com.momogo")
 @EntityScan(basePackages = "com.momogo.core.domain")
 @EnableJpaRepositories(basePackages = "com.momogo.core.domain")
